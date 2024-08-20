b82bd8548d15d6cc81c518f83c5dd4a077f9313e
Omaraitbenhaddi <omar.aitbenhaddio@outlook.fr>
2024-08-20 21:15:56 +0200

    kubernestss provisder auth

--- EKS/main.tf
+++ EKS/main.tf
@@ -54,3 +54,13 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
