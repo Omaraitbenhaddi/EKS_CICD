287b601b2679244d37b7c8977b4971b63bc8a827
Omaraitbenhaddi <omar.aitbenhaddio@outlook.fr>
2024-08-21 02:39:23 +0200

    A

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,18 +1,18 @@
-# VPC Module
+#Vpc
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eks_vpc"
+  name = "eks_cluster_vpc"
   cidr = var.eks_cidr
 
-  azs                = data.aws_availability_zones.azs.names
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  azs             = data.aws_availability_zones.azs.names
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 
-  enable_dns_hostnames    = true
-  map_public_ip_on_launch = true
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
   tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
@@ -22,7 +22,6 @@ module "vpc" {
     "kubernetes.io/role/elb"               = 1
 
   }
-
   private_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/private_elb"       = 1
@@ -30,23 +29,22 @@ module "vpc" {
   }
 }
 
-
-# eks 
+#EKS
 
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.30"
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     nodes = {
-      min_size        = 1
-      max_size        = 3
-      desired_size    = 2
-      instances_types = var.instances_types
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instances_types
     }
   }
   tags = {
@@ -55,12 +53,10 @@ module "eks" {
   }
 }
 
-
-
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
