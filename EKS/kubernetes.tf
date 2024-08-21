c5a88b870b89aead259d33ec614d3bef6c9e574b
Omaraitbenhaddi <omar.aitbenhaddio@outlook.fr>
2024-08-21 14:05:32 +0200

    creat deploiment

--- EKS/kubernetes.tf
+++ EKS/kubernetes.tf
@@ -3,3 +3,37 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
+
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "nginx"
+  }
+}
+
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+        }
+      }
+    }
+  }
+}
