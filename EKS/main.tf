a1e65b49f83f90f067d632321a492b2b27d543b7
Omaraitbenhaddi <omar.aitbenhaddio@outlook.fr>
2024-08-21 02:51:55 +0200

    A

--- EKS/main.tf
+++ EKS/main.tf
@@ -54,9 +54,9 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
